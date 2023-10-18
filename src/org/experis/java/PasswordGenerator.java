c46e34f3068c90ade7823e5a72ccde95cf3c17c1
andriy-kolokolov <koloandre22@gmail.com>
2023-10-18 17:59:34 +0200

    task password generator

--- src/org/experis/java/PasswordGenerator.java
+++ src/org/experis/java/PasswordGenerator.java
@@ -1,48 +1,42 @@
 package org.experis.java;
 
-import java.security.SecureRandom;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PasswordGenerator {
     public static void main(String[] args) {
-        int passLength = 0;
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            try {
-                System.out.print("Insert your secure password length: ");
-                passLength = scanner.nextInt();
-                break; // exit loop if a valid integer is provided
-            } catch (InputMismatchException e) {
-                System.err.println("\nInvalid input. Please enter a valid integer:");
-                scanner.nextLine();
-            }
-        }
+        System.out.println("Insert name");
+        String name = scanner.nextLine();
+        System.out.println("Insert lastname");
+        String lastname = scanner.nextLine();
+        System.out.println("Insert color");
+        String color = scanner.nextLine();
+        System.out.println("Insert date birth");
+        String dateBirth = scanner.nextLine();
+
         scanner.close();
 
-        System.out.println("Your secure password is: " + getSecurePassword(passLength));
+        System.out.println("Your secure password is: " + getPassword(name, lastname, color, dateBirth));
     }
 
-    public static String getSecurePassword(int len) {
-
-        SecureRandom random = new SecureRandom();
-        StringBuilder password = new StringBuilder();
-
-        long currentTimeMillis = System.currentTimeMillis();
-        String chars = "0123456789!@#$%^&*()-_+=<>?";
-        String lowerChars = "abcdefghijklmnopqrstuvwxyz";
-        String upperChars = lowerChars.toUpperCase();
-
-        String allChars = chars + lowerChars + upperChars + currentTimeMillis;
-
-        for (int i = 0; i < len; i++) {
-            // get random index in max length chars range
-            int randomIndex = random.nextInt(allChars.length());
-            // append random selected char
-            password.append(allChars.charAt(randomIndex));
+    public static String getPassword(
+            String name,
+            String lastname,
+            String color,
+            String dateBirth
+    ) {
+        // split and sum date nums
+        int dateSum = 0;
+        String[] dateSplit = dateBirth.split("/");
+        int[] numbers = new int[dateSplit.length];
+        for (int i = 0; i < dateSplit.length; i++) {
+            numbers[i] = Integer.parseInt(dateSplit[i]);
+        }
+        for (int number : numbers) {
+            dateSum += number;
         }
 
-        return password.toString();
+        return String.format("%s-%s-%s-%s", name, lastname, color, dateSum);
     }
 }
